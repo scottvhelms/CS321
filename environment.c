98afca37600365ba0d70a2704e4f9663daa05388
Scott <keeks@DESKTOP-I5VRF6L.localdomain>
2021-09-30 13:47:24 -0400

    Setting up lib skeletons

---
+++ environment.c
@@ -0,0 +1,1 @@
+#include "enviroment.h"
---
+++ main.c
@@ -0,0 +1,22 @@
+/*** INCLUDES ***/
+
+#include <stdlib.h>
+#include <SDL2/SDL.h>		// init and shutdown
+#include <SDL2/SDL_image.h>	// rendering and graphinc
+#include <SDL2/SDL_timer.h>	// delay and timing functions
+
+//libraries built by team 4
+#include "environment.h"
+#include "interactables.h"
+#include "character.h"
+
+
+
+
+int main(){
+
+
+
+	return 0;
+
+}
