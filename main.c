60def7f8a1003e7e52222d98131245ebf6f7390a
Scott Helms <scott.v.helms@gmail.com>
2021-10-12 16:32:11 -0400

    Added main.h and moved includes to main.h, removed from main.c

--- main.c
+++ main.c
@@ -1,16 +1,4 @@
-/*** INCLUDES ***/
-
-#include <stdlib.h>
-#include <SDL2/SDL.h>		// init and shutdown
-#include <SDL2/SDL_image.h>	// rendering and graphinc
-#include <SDL2/SDL_timer.h>	// delay and timing functions
-
-//libraries built by team 4
-#include "environment.h"
-#include "interactables.h"
-#include "character.h"
-
-
+#include "main.h"
 
 
 int main(){
