c39d8dcccd06dcfc0469e6a9e97d93c7e18c14ab
Scott Helms <scott.v.helms@gmail.com>
2021-10-12 16:40:53 -0400

    Added libraries.h and included in main.h and environment.h

---
+++ libraries.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+
--- main.h
+++ main.h
@@ -1,9 +1,1 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_timer.h>
-
-#include "environment.h"
-#include "interactables.h"
-#include "character.h"
+#include "libraries.h"
