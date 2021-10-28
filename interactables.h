ffccdcead651dd3ab217c292090feab2cd2ea2f2
JMendoza <35063857+JedMendozaAlcantara@users.noreply.github.com>
2021-10-28 17:32:35 -0400

    Update interactables.h

--- interactables.h
+++ interactables.h
@@ -4,9 +4,26 @@
  */
 
 enum interactables {
-	pickup,
-	dropoff
+	pickup = 1,
+	dropoff = 2
 };
+
+/*The purpose of this class is to
+ * define the interactable 
+ * Movement behaviors
+ */
+typedef struct {
+	//Map map;
+} Movement;
+
+/*The purpose of this class is to
+ * define the interactable 
+ * Action behaviors
+ */
+typedef struct {
+	enum interactables typeOfAction;
+} Action;
+
 
 /*The pupose of this class is to
  * define an interactable (npc)
@@ -14,16 +31,10 @@ enum interactables {
 typedef struct {
 	int positionX;
 	int positionY;
-	Event event;
+	Action action;
+	Movement movement;
 } Interactable;
 
-/*The purpose of this class is to
- * define the interactable behavior
- * based on the enum types
- */
-typedef struct {
-	enum interactables event;
-} Event;
-
-
-/*This is a test*/
+/*Interactable Methods*/
+void move(Interactable interactable, int newPX, int newPY);
+void act(Interactable interactable);
